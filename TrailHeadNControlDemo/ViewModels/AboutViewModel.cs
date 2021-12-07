ca4b3397e88b589c3dddb4c2509444dadf548cea
Walter Aguilar <walter.aguilar.zegarra@gmail.com>
2021-12-07 14:39:40 -0500

    Changing URL

--- TrailHeadNControlDemo/ViewModels/AboutViewModel.cs
+++ TrailHeadNControlDemo/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace TrailHeadNControlDemo.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://trailheadtechnology.com/"));
         }
 
         public ICommand OpenWebCommand { get; }
