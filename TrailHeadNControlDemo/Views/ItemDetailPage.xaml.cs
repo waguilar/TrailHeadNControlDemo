eac56c8796369c70da2f16f0468fa0af59cb23b0
Walter Aguilar <walter.aguilar.zegarra@gmail.com>
2021-12-07 11:03:15 -0500

    Initial Setup

---
+++ TrailHeadNControlDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TrailHeadNControlDemo.ViewModels;
+using Xamarin.Forms;
+
+namespace TrailHeadNControlDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TrailHeadNControlDemo/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TrailHeadNControlDemo.Models;
+using TrailHeadNControlDemo.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrailHeadNControlDemo.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
