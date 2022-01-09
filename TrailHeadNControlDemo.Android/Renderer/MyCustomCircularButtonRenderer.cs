07a8e4a4e720fb490d3a32f34d112bd4e9a5eda1
Walter Aguilar <walter.aguilar.zegarra@gmail.com>
2022-01-09 03:18:25 -0500

    Done!

---
+++ TrailHeadNControlDemo.Android/Renderer/MyCustomCircularButtonRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.Content;
+using TrailHeadNControlDemo.Controls;
+using TrailHeadNControlDemo.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyCustomCircularButton), typeof(MyCustomCircularButtonRenderer))]
+namespace TrailHeadNControlDemo.Droid.Renderer
+{
+    class MyCustomCircularButtonRenderer : ButtonRenderer
+    {
+        public MyCustomCircularButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                //Control.SetBackgroundDrawable(Control.Context.GetDrawable(Resource.Drawable.custom_renderer_shape)); //deprecated!
+                //Control.SetBackground(Control.Context.GetDrawable(Resource.Drawable.custom_renderer_shape)); //Not recomended as it is not fully supported
+                Control.Background = Control.Context.GetDrawable(Resource.Drawable.custom_renderer_shape);
+            }
+        }
+
+    }
+}
---
+++ TrailHeadNControlDemo/Controls/MyCustomCircularButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TrailHeadNControlDemo.Controls
+{
+    public class MyCustomCircularButton : Button
+    {
+        public MyCustomCircularButton()
+        {
+            
+        }
+    }
+}
